6405da73d8c71d1cba075546a407a0c194ee63b1
YuJiXYZ <17354006271@126.com>
2024-02-21 20:50:35 +0800

    add baseline config

---
+++ configs/cylinder3d/cylinder3d_1xb2-3x_semantickitti.py
@@ -0,0 +1,5 @@
+_base_ = [
+    'cylinder3d_4xb4-3x_semantickitti.py'
+]
+
+train_dataloader = dict(batch_size=2, )
---
+++ configs/cylinder3d/cylinder3d_1xb2-laser-polar-mix-3x_semantickitti.py
@@ -0,0 +1,3 @@
+_base_ = [
+   'cylinder3d_8xb2-laser-polar-mix-3x_semantickitti.py'
+]
